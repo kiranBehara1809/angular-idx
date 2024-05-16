7dfbdb7c05e6d9369b12820a92776a499914a65b
kiranbehara69 <kiranbehara69@gmail.com>
2024-05-16 03:16:33 +0000

    my initial change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/Auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)},
+{ path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)},
+{ path: 'layout', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule), canActivate: [AuthGuard] },
+];
